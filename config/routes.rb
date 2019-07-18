523e95df752be77b6d0e698ccbe5ac693bf6b38b
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-18 16:10:07 -0600

    Updates routes to satisfy merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   root 'welcome#index'
 
   get '/dashboard', to: 'users#show'
 
   get 'auth/strava/callback', to: 'login#create'
-=======
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #
- 
-	root "home#index"
 	
-	get '/auth/spotify/callback', to: 'sessions#create'
->>>>>>> rs_oauth_spike
+	get '/auth/spotify/callback', to: 'spotify#create'
 end
