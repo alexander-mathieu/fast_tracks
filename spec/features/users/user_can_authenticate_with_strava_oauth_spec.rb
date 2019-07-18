d5540eee2692336a674a30c368b0c746a7aa2987
William Homer <billyhomeriii@gmail.com>
2019-07-18 14:19:43 -0600

    add vcr for strava_get_athlete
    Co-Authored-By: Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>

--- spec/features/users/user_can_authenticate_with_strava_oauth_spec.rb
+++ spec/features/users/user_can_authenticate_with_strava_oauth_spec.rb
@@ -18,15 +18,12 @@ RSpec.describe 'As a visitor' do
           provider: 'strava',
           credentials: { token: ENV['STRAVA_TEST_TOKEN'] })
 
-        user = User.create!(strava_uid: 3214,
-                            strava_firstname: 'William',
-                            strava_lastname: 'Homer',
-                            strava_token: ENV['STRAVA_TEST_TOKEN'])
-
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
         visit root_path
-        click_button 'Login with Strava'
+        VCR.use_cassette('strava_get_athlete') do
+          click_button 'Login with Strava'
+        end
+
+        user = User.last
 
         expect(current_path).to eq(dashboard_path)
         expect(page).to have_content(user.strava_firstname)
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,16 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'vcr'
+require 'webmock/rspec'
+
+VCR.configure do |config|
+  config.ignore_localhost = true
+  config.cassette_library_dir = 'spec/cassettes'
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+  config.filter_sensitive_data('<STRAVA_TEST_TOKEN>') { ENV['STRAVA_TEST_TOKEN'] }
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
