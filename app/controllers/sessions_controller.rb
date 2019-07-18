c0cc8fb4c201cb04d6d9b0cdec8d007caa02be5f
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-18 15:53:36 -0600

    Adds figaro to sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,10 @@
 class SessionsController < ApplicationController
   def create
- 		client_id     = "80890ce6a3434c679442d9526f2e38b8"
-  	client_secret = "1fef22b83d1140b7a4fa861f0989e3b3"
+ 		client_id     = ENV['SPOTIFY_CLIENT_ID'] 
+  	client_secret = ENV['SPOTIFY_CLIENT_SECRET']  
   	code          = params[:code]
 		redirect_uri 	= "http://localhost:3000/auth/spotify/callback"
   	response      = Faraday.post("https://accounts.spotify.com/api/token?grant_type=authorization_code&client_id=#{client_id}&client_secret=#{client_secret}&code=#{code}&redirect_uri=#{redirect_uri}")
-		binding.pry
 		redirect_to root_path
   end
 end
