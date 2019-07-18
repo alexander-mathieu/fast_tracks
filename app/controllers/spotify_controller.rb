e9a953b340119f8df317a9eb5054cab5d88da22d
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-18 16:10:31 -0600

    renames spotify controller

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -1,10 +1,10 @@
-class SessionsController < ApplicationController
+class SpotifyController < ApplicationController
   def create
- 		client_id     = ENV['SPOTIFY_CLIENT_ID'] 
+ 	client_id     = ENV['SPOTIFY_CLIENT_ID'] 
   	client_secret = ENV['SPOTIFY_CLIENT_SECRET']  
   	code          = params[:code]
-		redirect_uri 	= "http://localhost:3000/auth/spotify/callback"
+	redirect_uri 	= "http://localhost:3000/auth/spotify/callback"
   	response      = Faraday.post("https://accounts.spotify.com/api/token?grant_type=authorization_code&client_id=#{client_id}&client_secret=#{client_secret}&code=#{code}&redirect_uri=#{redirect_uri}")
-		redirect_to root_path
+	redirect_to root_path
   end
 end
