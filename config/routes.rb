47088e8767f0fbfd2c9ec0d719b651c95c913362
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-17 20:39:51 -0600

    Add root_path, dashboard_path and strava callback route
    Co-Authored-By: William Homer <billyhomeriii@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+
+  get '/dashboard', to: 'users#show'
+
+  get 'auth/strava/callback', to: 'login#new'
 end
