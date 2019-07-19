d0f65b2b552b67158075d67baa56637057519fa4
William Homer <billyhomeriii@gmail.com>
2019-07-18 21:27:52 -0600

    add user factory

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    strava_uid { 1234 }
+    strava_firstname { "MyString" }
+    strava_lastname { "MyString" }
+    strava_token { "MyString" }
+    spotify_uid { "MyString" }
+    spotify_token { "MyString" }
+  end
+end
