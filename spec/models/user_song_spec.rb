107b24ad4b79bc96b5d1a3912ccb14ceec948cbe
William Homer <billyhomeriii@gmail.com>
2019-07-18 21:22:21 -0600

    add tests for user_songs belongs to

--- spec/models/user_song_spec.rb
+++ spec/models/user_song_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe UserSong, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should belong_to(:song) }
 end
