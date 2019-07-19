09a319c71add428fe43cc41e37b301bf6c722f8f
William Homer <billyhomeriii@gmail.com>
2019-07-18 22:37:39 -0600

    add songs show route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get '/dashboard', to: 'users#show'
+  resources :songs, only: [:show]
 
   get 'auth/strava/callback', to: 'login#create'
 end
