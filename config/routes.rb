889b88cae840994e036d7ef7b6a8865dd6040d58
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-20 09:04:18 -0600

    Adds cleaned up routes for spotify oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root 'welcome#index'
 
   get '/dashboard', to: 'users#show'
 
   get 'auth/strava/callback', to: 'login#create'
-	
-	get '/auth/spotify/callback', to: 'spotify#create'
+  get 'auth/spotify/callback', to: 'spotify#create'
 end
