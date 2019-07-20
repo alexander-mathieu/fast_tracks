e11319c0e984939e2e6c6621d50f1be6e6f0151a
William Homer <billyhomeriii@gmail.com>
2019-07-20 16:37:52 -0600

    add has_many activities on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 class User < ApplicationRecord
   has_many :user_songs
   has_many :songs, through: :user_songs
+  has_many :activities
 
   def top_songs(limit)
     songs
@@ -10,5 +11,12 @@ class User < ApplicationRecord
       .select('songs.*, user_songs.play_count, user_songs.power_ranking')
       .order(power_ranking: :DESC)
       .limit(limit)
+  end
+
+  def last_activity_id
+    activities
+      .order(strava_id: :DESC)
+      .pluck(:strava_id)
+      .first
   end
 end
