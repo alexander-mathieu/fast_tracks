e04233a88ef78d2c62ab4d9c2da986ea4408b7a9
William Homer <billyhomeriii@gmail.com>
2019-07-20 10:29:12 -0600

    resolve merge conflicts

--- app/facades/dashboard_show_facade.rb
+++ app/facades/dashboard_show_facade.rb
@@ -6,4 +6,12 @@ class DashboardShowFacade
   def initialize(current_user)
     @top_songs = current_user.top_songs(10)
   end
+
+  def build_link
+    link = 'https://accounts.spotify.com/authorize?'
+    client_pair = "client_id=#{ENV['SPOTIFY_CLIENT_ID']}&"
+    code_pair = 'response_type=code&'
+    redirect_pair = 'redirect_uri=http://localhost:3000/auth/spotify/callback'
+    link + client_pair + code_pair + redirect_pair
+  end
 end
