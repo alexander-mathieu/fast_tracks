be8709cdaf05707a2bfc809b9c0556492711cc4b
William Homer <billyhomeriii@gmail.com>
2019-07-21 11:46:24 -0600

    removes duplicate build_link method
    41843577+Mycobee@users.noreply.github.com

--- app/facades/dashboard_show_facade.rb
+++ app/facades/dashboard_show_facade.rb
@@ -26,12 +26,4 @@ class DashboardShowFacade
   def user_name
     @user.strava_firstname + ' ' + @user.strava_lastname
   end
-
-  def build_link
-    link = 'https://accounts.spotify.com/authorize?'
-    client_pair = "client_id=#{ENV['SPOTIFY_CLIENT_ID']}&"
-    code_pair = 'response_type=code&'
-    redirect_pair = 'redirect_uri=http://localhost:3000/auth/spotify/callback'
-    link + client_pair + code_pair + redirect_pair
-  end
 end
