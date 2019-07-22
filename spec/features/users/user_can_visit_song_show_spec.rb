8cb71e8dc417752810297db476ace85c0b5c56e9
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-22 16:36:14 -0600

    Adds initial spec for page to be built out

--- spec/features/users/user_can_visit_song_show_spec.rb
+++ spec/features/users/user_can_visit_song_show_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe "As a fully connected user on my dashboard path" do
 	describe "I click a song link" do
 		it "Takes me to a show page with title, metrics, album cover, and 5 activities" do
-			sp_token = "bullshit_token"
-			st_token = "bullshit_token"
+			sp_token = "fake_token"
+			st_token = "fake_token"
 			song = create(:song) 
 			user = create(:user, spotify_token: sp_token, strava_token: st_token)
 			user_song = create(:user_song, user_id: user.id, song_id: song.id) 
@@ -15,8 +15,13 @@ describe "As a fully connected user on my dashboard path" do
 			click_link(song.title)
 			expect(current_path).to eq(song_path(song))
 
-			expect(page).to have_content("Title: #{song.title}")
-			expect(page).to have_selector('#album_art')
+			within "#song-title" do
+				expect(page).to have_content(song.title)
+			end
+save_and_open_page
+			expect(page).to have_selector('#album-art')
+			expect(page).to have_selector('#song-metrics')
+			expect(page).to have_selector('#last-activities')
 		end
 	end
 end
