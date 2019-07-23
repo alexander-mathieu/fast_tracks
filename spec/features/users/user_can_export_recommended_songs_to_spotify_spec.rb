8f82f5ec40e4bbb1c1002e1dec73de097de5dc36
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-23 17:53:44 -0600

    Update cassette and spec

--- spec/features/users/user_can_export_recommended_songs_to_spotify_spec.rb
+++ spec/features/users/user_can_export_recommended_songs_to_spotify_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe 'As a registered user' do
   describe 'when I visit my dashboard' do
     before :each do
-      @user = create(:user, spotify_uid: ENV['SPOTIFY_UID'], spotify_token: ENV['SPOTIFY_TEST_TOKEN'])
+      @user = create(:user, spotify_uid: 'billyiii', spotify_token: '12345')
 
       create(:user_song, user: @user, power_ranking: 0.9, song: create(:song, spotify_id: '2MIcpZ7MBeCUEVFDBqU7Ei'))
       create(:user_song, user: @user, power_ranking: 0.8, song: create(:song, spotify_id: '4v6dF5830rtgjYr0uov248'))
