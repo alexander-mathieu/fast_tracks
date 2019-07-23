3c992d3ec3927f13347fb5be56ddd9ebf2c9bf36
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-23 14:26:16 -0600

    Refactor SpotifyService calls in controller, fix flash message content

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -5,15 +5,26 @@ class SpotifyController < ApplicationController
   def create
     token_info = SpotifyOauthGenerator.new(params['code']).connect_to_spotify
     current_user.spotify_token = token_info['access_token']
-    current_user.spotify_uid = params[:id]
+    current_user.spotify_uid = user_info[:id]
     current_user.save
-    flash[:success] = "Spotify data synced"
+    flash[:success] = 'Connected to Spotify!'
     redirect_to dashboard_path
   end
 
   def index
-    songs_data = SpotifyService.new(current_user.spotify_token).get_user_songs[:items]
+    songs_data = spotify_service.get_user_songs[:items]
     SongSifter.new(songs_data, current_user).sift_songs
+    flash[:success] = 'Spotify data synced!'
     redirect_to dashboard_path
+  end
+
+  private
+
+  def user_info
+    spotify_service.get_user_info
+  end
+
+  def spotify_service
+    @spotify_service ||= SpotifyService.new(current_user.spotify_token)
   end
 end
