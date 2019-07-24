f0fe11278501ebad1b052af95e43c76be5c75a27
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-24 14:32:12 -0600

    Adds jquery ajax request for refresh recommended songs

---
+++ app/assets/javascripts/refresh_recommended.js
@@ -0,0 +1,10 @@
+
+
+//	$.ajax({
+//	  type: "POST", 
+//	  url: "/things",
+//	  data: mydata,
+//	  success: function(data, textStatus, jqXHR){...},
+//	  error: function(jqXHR, textStatus, errorThrown){...}
+//	})								
+//	console.log(document);
