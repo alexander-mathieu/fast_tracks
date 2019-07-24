5a530949615308c06322e0f8f45eb35f05ed0b6e
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-24 16:15:54 -0600

    Add instance methods tests for Song model, for #formatted_length

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -3,5 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  it { should have_many(:users) }
+  context 'relationships' do
+    it { should have_many(:users) }
+  end
+
+  context 'instance methods' do
+    it '#formatted_length' do
+      song = create(:song, length: 143397)
+      expect(song.formatted_length).to eq('2:23')
+    end
+  end
 end
