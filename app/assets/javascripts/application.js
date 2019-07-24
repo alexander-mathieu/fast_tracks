ca6735cfe7740b4d80a5089d0634037c09bda76a
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-24 14:35:07 -0600

    Adds jquery ajax request to flask microservice

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,34 @@
 //
 //= require rails-ujs
 //= require activestorage
+//= require jquery3
+//= require jquery_ujs
 //= require_tree .
+//  var dataid = $(this).attr('data-id');
+//  var id = $(this).closest('.test').attr('id');
+
+$(document).ready(function(){
+	$("#refresh-recs").click(function(){
+		var params = $('#refresh-recs').attr('data_api_params')
+    $.ajax({
+     type: "Get",
+     contentType: "application/json; charset=utf-8",
+     url: "https://fast-tracks-flask.herokuapp.com/api/v1/recommended",
+     data : params,
+     dataType: "json",
+     success: function (new_recommended) {
+				var i 
+				for (i = 0; i < 5; i++) {
+					$('.recommended-iframe')[i].src = 'https://open.spotify.com/embed/track/' + new_recommended[i].spotify_id
+				}
+				//for var_i = 0 to new_recommended.length
+
+				//set the song of each source by index i
+				console.log("success!!")
+     },
+     error: function (){
+				console.log("something wrong!!")
+     }
+    });
+  });
+});
