8f6f3ca507bad99b5c9a89273a5b17da8a981b6a
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-24 17:11:18 -0600

    Creates song_sync job

--- app/jobs/song_sync_job.rb
+++ app/jobs/song_sync_job.rb
@@ -1,7 +1,9 @@
 class SongSyncJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
-  end
+	def perform(current_user)
+		spotify_service = SpotifyService.new(current_user.spotify_token)
+    songs_data = spotify_service.get_user_songs[:items]
+    SongSifter.new(songs_data, current_user).sift_songs
+	end
 end
