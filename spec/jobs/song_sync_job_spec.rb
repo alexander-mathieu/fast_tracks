590b771075aeb9127e81deca2cf01c87f510d86a
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-24 18:02:26 -0600

    Adds spec for song_sync_job

--- spec/jobs/song_sync_job_spec.rb
+++ spec/jobs/song_sync_job_spec.rb
@@ -1,5 +1,16 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe SongSyncJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "will sync a user's songs" do
+    VCR.use_cassette('song_sync_job') do
+      token = 'BQBN3pCzyhCqLxyZiIRalIvA2aN9gN9sQiPZhrl6CHa-uzErubDT7s57coRkhazXSOJuOZirZI72sN_8KsLl_TX1-9ybaZfmTqkcfOTEVOt7KHbLD9FCHczgyx2jyW6YOIwumSIlYMXSHT1XJQGuc3PV-CSl_KfHkkAhCjnpTwYATuqNmo5V-4I'
+      user = create(:user, spotify_token: token)
+
+      SongSyncJob.perform_now(user)
+      user.reload
+      expect(user.user_songs.count).to eq(50)
+    end
+  end
 end
