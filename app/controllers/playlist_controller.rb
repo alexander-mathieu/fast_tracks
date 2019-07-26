bd85c62894cc7c2130f65929b0296c6b552a5295
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-26 11:07:33 -0600

    Add logic to create/find FastTracks playlist to PlaylistController create action with supporting methods

--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -2,10 +2,10 @@
 
 class PlaylistController < ApplicationController
   def create
-    if playlists_include_new_playlist?
-      playlist = build_playlist(params[:playlist_name])
+    if playlists_include_fasttracks?
+      playlist = fasttracks_playlist
     else
-      playlist = create_playlist(current_user.spotify_uid, params[:playlist_name])
+      playlist = create_fasttracks_playlist(current_user.spotify_uid)
     end
     spotify_service.add_songs_to_playlist(playlist[:id], params[:song_uris])
     flash[:success] = 'Playlist successfully added!'
@@ -14,21 +14,18 @@ class PlaylistController < ApplicationController
 
   private
 
-  def create_playlist(spotify_user_id, playlist_name)
-    spotify_service.create_user_playlist(spotify_user_id, playlist_name)
+  def create_fasttracks_playlist(spotify_user_id)
+    spotify_service.create_fasttracks_playlist(spotify_user_id)
   end
 
-  def build_playlist(playlist_name)
-    playlist_object = {}
-    playlists = find_playlists(current_user.spotify_uid, params[:playlist_name])
-    playlist_id = playlists.find { |playlist| playlist[:name] == playlist_name }[:id]
-    playlist_object[:id] = playlist_id
-    playlist_object
-  end
-
-  def playlists_include_new_playlist?
+  def fasttracks_playlist
     playlists = find_playlists
-    playlists.any? { |playlist| playlist[:name] == params[:playlist_name] }
+    playlists.find { |playlist| playlist[:name] == 'FastTracks' }
+  end
+
+  def playlists_include_fasttracks?
+    playlists = find_playlists
+    playlists.any? { |playlist| playlist[:name] == 'FastTracks' }
   end
 
   def find_playlists
